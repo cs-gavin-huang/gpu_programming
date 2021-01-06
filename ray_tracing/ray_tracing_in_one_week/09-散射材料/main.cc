188d10ad75ba32c12b7b23ecac7eb22c5dbec1dd
you-know-who-2017 <1376397133@qq.com>
2021-01-07 00:47:16 +0800

    update

--- ray_tracing/ray_tracing_in_one_week/09-散射材料/main.cc
+++ ray_tracing/ray_tracing_in_one_week/09-散射材料/main.cc
@@ -1,10 +1,10 @@
 /*
  * @Author: geekli
  * @Date: 2021-01-06 23:55:02
- * @LastEditTime: 2021-01-07 00:40:22
+ * @LastEditTime: 2021-01-07 00:45:52
  * @LastEditors: your name
  * @Description: 
- * @FilePath: /ray_tracing/ray_tracing_in_one_week/08-抗锯齿/main.cc
+ * @FilePath: /ray_tracing/ray_tracing_in_one_week/09-散射材料/main.cc
  */
 //inclue 结构发生变化
 #include "rtweekend.hpp"
@@ -14,10 +14,18 @@
 #include "camera.hpp"
 
 #include <iostream>
-color ray_color(const ray& r, const hittable& world) {
+
+color ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
+
+    // 光线放射限制，不再收集更多光线
+    if (depth <= 0)
+        return color(0,0,0);
+
     if (world.hit(r, 0, infinity, rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        //递归将深度减一
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
@@ -31,6 +39,8 @@ int main() {
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
+    //深度50
+    const int max_depth = 50;
 
     // World
     hittable_list world;
@@ -52,7 +62,8 @@ int main() {
                 auto u = (i + random_double()) / (image_width-1);
                 auto v = (j + random_double()) / (image_height-1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                //参数加上深度
+                pixel_color += ray_color(r, world, max_depth);
             }
             //标准化输出颜色 + 抗锯齿
             write_color(std::cout, pixel_color, samples_per_pixel);
